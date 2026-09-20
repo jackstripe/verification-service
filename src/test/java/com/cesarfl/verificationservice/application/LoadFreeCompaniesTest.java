8ce0426d8e776f5b732ba4eb76bf15a5304c17f8
Cesar Flores Lovera <ingcesarfl@gmail.com>
2026-09-20 01:56:42 -0600

    feat: add provider availability and premium search

--- src/test/java/com/cesarfl/verificationservice/application/LoadFreeCompaniesTest.java
+++ src/test/java/com/cesarfl/verificationservice/application/LoadFreeCompaniesTest.java
@@ -1,6 +1,7 @@
 package com.cesarfl.verificationservice.application;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import java.time.LocalDate;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
@@ -8,6 +9,7 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 @SpringBootTest
 class LoadFreeCompaniesTest {
 
+    @Autowired
     LoadFreeCompanies loadFreeCompanies;
 
     @Test
--- src/test/java/com/cesarfl/verificationservice/application/LoadPremiumCompaniesTest.java
+++ src/test/java/com/cesarfl/verificationservice/application/LoadPremiumCompaniesTest.java
@@ -1,6 +1,7 @@
 package com.cesarfl.verificationservice.application;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
@@ -9,6 +10,7 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 @SpringBootTest
 class LoadPremiumCompaniesTest {
 
+    @Autowired
     LoadPremiumCompanies loadPremiumCompanies;
 
     @Test
